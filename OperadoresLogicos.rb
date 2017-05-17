b33d515f1c4f37079ef4c8b47611175bf3b1ae68
JerryMtezH <gerardom.martinezh@gmail.com>
2017-05-17 16:31:32 -0500

    Commit 7 (Lunes Ej. Operadores Logicos)

---
+++ OperadoresLogicos.rb
@@ -0,0 +1,6 @@
+valor_1 = (1 < 7 || false) && (true || true)
+puts "El Valor de 1 es: #{valor_1}"
+valor_2 = !false && (!false || 50 != 5**10)
+puts "El Valor de 1 es: #{valor_2}"
+valor_3 = false || !(true || true)
+puts "El Valor de 1 es: #{valor_3}"
